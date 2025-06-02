ab3ff12a57dcdcda60cf540d3ea003681c402049
changelater1 <tereschenkovl11@gmail.com>
2025-06-02 21:44:47 +0300

    discovery+gateway+userservice works good

--- discovery-service/src/main/java/com/astonlabs/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/astonlabs/DiscoveryServiceApplication.java
@@ -2,11 +2,12 @@ package com.astonlabs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-
-public class Application {
+@EnableEurekaServer
+public class DiscoveryServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
     }
 }
